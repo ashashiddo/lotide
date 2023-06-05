8fed4d0ce38e1cb689ca63dc4018348fb9cea54a
ashashiddo <53973974+ashashiddo@users.noreply.github.com>
2023-06-05 17:24:59 -0400

    refactored tail.js and created tailTest.js

---
+++ test/tailTest.js
@@ -0,0 +1,15 @@
+const assertEqual = require('../assertEqual');
+
+const tail = require('../tail');
+
+
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
