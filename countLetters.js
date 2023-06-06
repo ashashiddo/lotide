1b33ceb33fa8ea3c617f6b33a701651fb9ba4a02
ashashiddo <53973974+ashashiddo@users.noreply.github.com>
2023-06-05 20:42:21 -0400

    add countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+};
+
+const countLetters = function(str) {
+  const result = {};
+
+  for (const letter of str) {
+    if (letter === ' ') {
+      continue;
+    }
+    if (!result[letter]) {
+      result[letter] = 0;
+    }
+    result [letter]++;
+  }
+  return result;
+};
+
+
+// Test cases
+const result1 = countLetters("Lighthouse");
+assertEqual(result1["L"], 1);
+assertEqual(result1["i"], 1);
+assertEqual(result1["g"], 1);
+assertEqual(result1["h"], 2);
+assertEqual(result1["t"], 1);
+assertEqual(result1["o"], 1);
+assertEqual(result1["u"], 1);
+assertEqual(result1["s"], 1);
+assertEqual(result1["e"], 1);
