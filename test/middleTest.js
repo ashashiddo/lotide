82a0180a6f2dcd742da6e49ee3c4d564d78a35e3
ashashiddo <53973974+ashashiddo@users.noreply.github.com>
2023-06-05 22:28:43 -0400

    M & C middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,25 @@
-const eqArrays = require('../eqArrays');
-
-const assertArraysEqual = require('../assertArraysEqual');
-
+const chai = require('chai');
+const assert = chai.assert;
 const middle = require('../middle');
 
-// Test
-assertArraysEqual(middle([1]), []); // Pass
-assertArraysEqual(middle([1, 2]), []); // Pass
-assertArraysEqual(middle([1, 2, 3]), [2]); // Pass
+describe('middle', () => {
+    it('returns []', () => {
+      const result = middle([1]);
+      assert.deepEqual(result, []);
+    });
+  
+    it('returns []', () => {
+      const result = middle([1, 2]);
+      assert.deepEqual(result, []);
+    });
+  
+    it('returns [2]', () => {
+      const result = middle([1, 2, 3]);
+      assert.deepEqual(result, [2]);
+    });
+  });
+
+// // Test
+// assertArraysEqual(middle([1]), []); // Pass
+// assertArraysEqual(middle([1, 2]), []); // Pass
+// assertArraysEqual(middle([1, 2, 3]), [2]); // Pass
