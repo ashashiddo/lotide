9a36b1bfaa17e35c4ae40c58c508fc93ae2c2ac0
ashashiddo <53973974+ashashiddo@users.noreply.github.com>
2023-06-06 01:01:50 -0400

    refactored takeUntil.js and created takeUnitlTest.js

--- takeUntil.js
+++ takeUntil.js
@@ -1,22 +1,5 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-  
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
   
 const takeUntil = function(array, callback) {
   const results = [];
@@ -30,14 +13,4 @@ const takeUntil = function(array, callback) {
   return results;
 };
     
-// Test1
-    
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-assertArraysEqual(results1, [1, 2, 5, 7, 2]);
-    
-//Test2
-    
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
+module.exports = takeUntil;
