1c8f6f6d153e08c004b5a203cedcaf63f157f199
ashashiddo <53973974+ashashiddo@users.noreply.github.com>
2023-06-05 22:21:32 -0400

    M & C tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,36 @@
-const assertEqual = require('../assertEqual');
-
+const chai = require('chai');
+const assert = chai.assert;
 const tail = require('../tail');
 
+describe('tail', () => {
+  it('should return an array with the tail elements', () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.deepEqual(result, ["Lighthouse", "Labs"]);
+    assert.strictEqual(result.length, 2);
+    assert.strictEqual(result[0], "Lighthouse");
+    assert.strictEqual(result[1], "Labs");
+  });
+  
+  it('should not modify the original array', () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.strictEqual(words.length, 3);
+    assert.strictEqual(words[0], "Yo Yo");
+    assert.strictEqual(words[1], "Lighthouse");
+    assert.strictEqual(words[2], "Labs");
+  });
+});
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+
+
+// // Test Case 1: Check the returned array elements
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2);
+// assertEqual(result[0], "Lighthouse");
+// assertEqual(result[1], "Labs");
+
+// // Test Case: Check the original array
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3);
