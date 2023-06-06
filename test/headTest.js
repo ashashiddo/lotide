3a0a2da0d3d0b8a5db24dc94e3a40ff3d07fe431
ashashiddo <53973974+ashashiddo@users.noreply.github.com>
2023-06-05 21:49:58 -0400

    coverted headTest.js to use M & C

--- test/headTest.js
+++ test/headTest.js
@@ -1,10 +1,21 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 
 const head = require('../head');
 
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
+
+it("returns '5' for ['5']", () => {
+  assert.strictEqual(head(['5']), '5');
+});
+
 // Test cases
 
-assertEqual(head([5,6,7]), 5); // ✅✅✅ Assertion Passed: 5 === 5
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // ✅✅✅ Assertion Passed: Hello === Hello
-assertEqual(head([21]), 21); // ✅✅✅ Assertion Passed: 21 === 21
-assertEqual(head([]), undefined); // ✅✅✅ Assertion Passed: undefined === undefined
+// assertEqual(head([5,6,7]), 5); // ✅✅✅ Assertion Passed: 5 === 5
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // ✅✅✅ Assertion Passed: Hello === Hello
+// assertEqual(head([21]), 21); // ✅✅✅ Assertion Passed: 21 === 21
+// assertEqual(head([]), undefined); // ✅✅✅ Assertion Passed: undefined === undefined
