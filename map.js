b298e78a2b506828c35ab5cfec944548c398884e
ashashiddo <53973974+ashashiddo@users.noreply.github.com>
2023-06-05 21:23:08 -0400

    add map.js

---
+++ map.js
@@ -0,0 +1,42 @@
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+
+//TEST
+
+const expected = [6, 7, 2, 5, 3];
+const actual = map(words, word => word.length);
+assertArraysEqual(actual, expected); // Pass
